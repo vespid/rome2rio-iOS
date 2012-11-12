e464254d300a58865693568677813978fe792a1c
rome2rio <git@rome2rio.com>
2012-11-12 19:11:11 +1100

    Cleaned code

---
+++ Rome2Rio/Rome2Rio/R2RCompletionState.h
@@ -0,0 +1,14 @@
+//
+//  R2RCompletionState.h
+//  Rome2Rio
+//
+//  Created by Ash Verdoorn on 12/11/12.
+//  Copyright (c) 2012 Rome2Rio. All rights reserved.
+//
+
+#ifndef Rome2Rio_R2RCompletionState_h
+#define Rome2Rio_R2RCompletionState_h
+
+
+
+#endif
