d4a994c740ae879328ed9d8365e9d34e8762eda6
rome2rio <git@rome2rio.com>
2012-11-30 15:07:43 +1100

    MapView Changes
    Added zoom to "To" field location on viewDidLoad
    Added tap gesture to place pin for current field
    Added long press gesture to display to/from here annotation

--- Rome2Rio/Rome2Rio/R2RMapViewController.m
+++ Rome2Rio/Rome2Rio/R2RMapViewController.m
@@ -45,17 +45,29 @@
     [self.statusButton addTarget:self action:@selector(statusButtonClicked) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:self.statusButton];
     
-    //temp area for Melbourne zoom
-    CLLocationCoordinate2D coord = CLLocationCoordinate2DMake(-37.816022 ,144.96151);
-    MKCoordinateSpan span = MKCoordinateSpanMake(0.2, 0.2);
-    MKCoordinateRegion region = MKCoordinateRegionMake(coord, span);
+//    //temp area for Melbourne zoom
+//    CLLocationCoordinate2D coord = CLLocationCoordinate2DMake(-37.816022 ,144.96151);
+//    MKCoordinateSpan span = MKCoordinateSpanMake(0.2, 0.2);
+//    MKCoordinateRegion region = MKCoordinateRegionMake(coord, span);
+
+    MKCoordinateRegion region = MKCoordinateRegionForMapRect(MKMapRectWorld);
     
+    if ([self.fieldName isEqualToString:@"to"] && self.searchManager.searchStore.fromPlace)
+    {
+        CLLocationCoordinate2D fromCoord = CLLocationCoordinate2DMake(self.searchManager.searchStore.fromPlace.lat , self.searchManager.searchStore.fromPlace.lng);
+        [self setFromLocation:fromCoord];
+        region = MKCoordinateRegionMakeWithDistance(fromCoord , 50000, 50000);
+    }
+        
     [self.mapView setRegion:region];
     
-//    [self.mapView setRegion:MKCoordinateRegionForMapRect(MKMapRectWorld)];
+    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(setAnnotationForTap:)];
+    [self.mapView addGestureRecognizer:tapGesture];
+    tapGesture.delegate = self;
     
     UILongPressGestureRecognizer *longPressGesture = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(showPressAnnotation:)];
     [self.mapView addGestureRecognizer:longPressGesture];
+    longPressGesture.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning
@@ -72,9 +84,14 @@
 - (IBAction)resolveLocation:(id)sender
 {
     //if no location is currently selected display instructions
-    if (!self.fromAnnotation && !self.toAnnotation)
+    if ([self.fieldName isEqualToString:@"from"] && !self.fromAnnotation)
     {
-        [self.statusButton setTitle:@"Press and hold to select location" forState:UIControlStateNormal];
+        [self.statusButton setTitle:@"Select origin" forState:UIControlStateNormal];
+        return;
+    }
+    else if ([self.fieldName isEqualToString:@"to"] && !self.toAnnotation)
+    {
+        [self.statusButton setTitle:@"Select destination" forState:UIControlStateNormal];
         return;
     }
     
@@ -94,6 +111,11 @@
         [self.searchManager setToWithMapLocation:self.toAnnotation.coordinate mapScale:mapScale];
     }
 
+    [self dismissModalViewControllerAnimated:YES];
+}
+
+- (IBAction)returnToSearch:(id)sender
+{
     [self dismissModalViewControllerAnimated:YES];
 }
 
@@ -150,11 +172,11 @@
     {
         R2RPressAnnotationView *pressAnnotationView = (R2RPressAnnotationView *)annotationView;
         [pressAnnotationView.fromButton addTarget:self
-                                         action:@selector(setFromLocation:)
+                                         action:@selector(setFromLocationFromLongPress:)
                                forControlEvents:UIControlEventTouchUpInside];
         
         [pressAnnotationView.toButton addTarget:self
-                                       action:@selector(setToLocation:)
+                                       action:@selector(setToLocationFromLongPress:)
                              forControlEvents:UIControlEventTouchUpInside];
         
         return pressAnnotationView;
@@ -166,37 +188,75 @@
     return annotationView;
 }
 
--(void) setFromLocation:(id) sender
+- (void)setAnnotationForTap :(UITapGestureRecognizer *)gestureRecognizer
 {
-    if (!self.fromAnnotation)
+    if (self.pressAnnotation)
     {
-        //TODO maybe add a name here so magnifying glass is shown and then set it to zoom to different levels
-        self.fromAnnotation = [[R2RAnnotation alloc] initWithName:nil kind:nil coordinate:self.pressAnnotation.coordinate annotationType:r2rAnnotationTypeFrom];
-        [self.mapView addAnnotation:self.fromAnnotation];
+        [self.mapView deselectAnnotation:self.pressAnnotation animated:YES];
     }
-    else
+    
+    [self.statusButton setTitle:nil forState:UIControlStateNormal];
+    
+    CGPoint touchPoint = [gestureRecognizer locationInView:self.mapView];
+    CLLocationCoordinate2D touchMapCoordinate = [self.mapView convertPoint:touchPoint toCoordinateFromView:self.mapView];
+    
+    if ([self.fieldName isEqualToString:@"from"])
     {
-        [self.fromAnnotation setCoordinate:self.pressAnnotation.coordinate];
+        [self setFromLocation:touchMapCoordinate];
     }
+    if ([self.fieldName isEqualToString:@"to"])
+    {
+        [self setToLocation:touchMapCoordinate];
+    }
+}
 
+-(void) setFromLocationFromLongPress:(id) sender
+{
+    [self setFromLocation:self.pressAnnotation.coordinate];
+    
     [self.mapView removeAnnotation:self.pressAnnotation];
     self.pressAnnotation = nil;
 }
 
--(void) setToLocation:(id) sender
+-(void) setToLocationFromLongPress:(id) sender
+{
+    [self setToLocation:self.pressAnnotation.coordinate];
+    
+    [self.mapView removeAnnotation:self.pressAnnotation];
+    self.pressAnnotation = nil;
+}
+
+-(void) setFromLocation:(CLLocationCoordinate2D) coord
+{
+    if (!self.fromAnnotation)
+    {
+        //TODO maybe add a name here so magnifying glass is shown and then set it to zoom to different levels
+        self.fromAnnotation = [[R2RAnnotation alloc] initWithName:nil kind:nil coordinate:coord annotationType:r2rAnnotationTypeFrom];
+        [self.mapView addAnnotation:self.fromAnnotation];
+    }
+    else
+    {
+        [self.fromAnnotation setCoordinate:coord];
+    }
+}
+
+-(void) setToLocation:(CLLocationCoordinate2D) coord
 {
     if (!self.toAnnotation)
     {
-        self.toAnnotation = [[R2RAnnotation alloc] initWithName:nil kind:nil coordinate:self.pressAnnotation.coordinate annotationType:r2rAnnotationTypeTo];
+        self.toAnnotation = [[R2RAnnotation alloc] initWithName:nil kind:nil coordinate:coord annotationType:r2rAnnotationTypeTo];
         [self.mapView addAnnotation:self.toAnnotation];
     }
     else
     {
-        [self.toAnnotation setCoordinate:self.pressAnnotation.coordinate];
+        [self.toAnnotation setCoordinate:coord];
     }
-    
-    [self.mapView removeAnnotation:self.pressAnnotation];
-    self.pressAnnotation = nil;
+}
+
+#pragma mark - UIGestureRecognizerDelegate
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldReceiveTouch:(UITouch *)touch
+{
+    return ![touch.view isKindOfClass:[UIButton class]];
 }
 
 @end
